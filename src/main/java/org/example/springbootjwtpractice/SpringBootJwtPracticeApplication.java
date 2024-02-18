6f78dbc0ab5125644fc2c5e8c54f6005405a4ba1
Labesh <labheshwar.work@gmail.com>
2024-02-18 16:12:41 +0500

    Initial commit

---
+++ src/main/java/org/example/springbootjwtpractice/SpringBootJwtPracticeApplication.java
@@ -0,0 +1,13 @@
+package org.example.springbootjwtpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJwtPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJwtPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/springbootjwtpractice/SpringBootJwtPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springbootjwtpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJwtPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
